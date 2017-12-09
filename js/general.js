656a4c3b12b3bc52db78b8150be6c1c393afa9b3
Jonathan <wackson86.jm@gmail.com>
2017-12-09 16:12:48 -0400

    proyecto fundacion energy 1er commit

---
+++ js/general.js
@@ -0,0 +1,9 @@
+function mostrar(){
+    document.getElementById("camino").style.display = "block";
+}
+function mostrar1(){
+    document.getElementById("camino1").style.display = "block";
+}
+function mostrar2(){
+    document.getElementById("camino2").style.display = "block";
+}
